c78a97963bbecc5909299668f0905b7ea39ce3b3
Edson Garcia <ednosmab@yahoo.com.br>
2019-05-22 09:53:09 -0300

    Dois repositorios contendo exercicios de fixacao dos cursos de PHP e JavaScript

--- ECMAScript/EXERCÍCIOS-JS/funcao/cidadaoPrimeiraClasse.js
+++ ECMAScript/EXERCÍCIOS-JS/funcao/cidadaoPrimeiraClasse.js
@@ -1,7 +1,7 @@
-//Função em JS é First-Class Object (Citizens)
+﻿//Função em JS é First-Class Object (Citizens)
 //Higher-order function
 
-//Criar deforma literal
+//Criar de forma literal
 function fun1() {}
 
 //Armazenar em uma variável
