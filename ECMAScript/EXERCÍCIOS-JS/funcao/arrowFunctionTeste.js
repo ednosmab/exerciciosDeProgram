f16d7240696fa75d5b25743fd66c0dd83e631552
Edson Garcia <ednosmab@yahoo.com.br>
2019-05-29 11:32:00 -0300

    Revert "Alterei o arquivo arrowFunctionTeste.js, removi o ultimo parenteses, ocorreu um erro de sintaxe"
    This reverts commit 4134007611309f3900472fe7e1d073b9d37e5b6a.

--- ECMAScript/EXERCÍCIOS-JS/funcao/arrowFunctionTeste.js
+++ ECMAScript/EXERCÍCIOS-JS/funcao/arrowFunctionTeste.js
@@ -2,4 +2,4 @@ const texto = () => 'Olá! '
 const texto2 = () => 'Bom dia! '
 const texto3 = nome => nome
 const imprimir = (a, b, c) => console.log(a + b + c)
-imprimir(texto(), texto2(), 'Edson'
+imprimir(texto(), texto2(), 'Edson')
