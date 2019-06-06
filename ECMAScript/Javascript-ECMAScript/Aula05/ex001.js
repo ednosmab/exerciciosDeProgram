7a49c25b8a9715c97cbfbfa401f18aea684dc754
Edson Garcia <ednosmab@yahoo.com.br>
2019-06-06 12:32:07 -0300

    Inserindo novos exercicios

---
+++ ECMAScript/Javascript-ECMAScript/Aula05/ex001.js
@@ -0,0 +1,26 @@
+//Utilizando typeof para verficar qual o tipo primitivo na declaração de variáveis
+let num, texto, objeto, funcao, array
+
+num = 10
+console.log(typeof(num))
+
+texto = "Google"
+console.log(typeof(texto))
+
+objeto = {}
+console.log(typeof(objeto))
+
+funcao = function(){}
+console.log(typeof(funcao))
+
+array = []
+console.log(typeof(array))
+
+texto = null
+console.log(typeof(texto))
+
+texto = NaN
+console.log(typeof(texto))
+
+texto = undefined
+console.log(typeof(texto))
